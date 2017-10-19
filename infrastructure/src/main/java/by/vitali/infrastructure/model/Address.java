8318a65438a50dfd423a016317fc62d75f53b148
Vitali Podelinski <viivpo2014@gmail.com>
2017-10-19 16:43:39 +0300

    my commit

--- infrastructure/src/main/java/by/vitali/infrastructure/model/Address.java
+++ infrastructure/src/main/java/by/vitali/infrastructure/model/Address.java
@@ -1,8 +1,7 @@
 package by.vitali.infrastructure.model;
 
 
-import lombok.Getter;
-import lombok.Setter;
+
 
 import javax.persistence.Column;
 import java.io.Serializable;
@@ -10,14 +9,37 @@ import java.io.Serializable;
 /**
  * This class save hotels address.
  */
-@Getter
-@Setter
+
 public class Address implements Serializable {
 
     private static final long serialVersionUID = 5274743189482560061L;
 
     @Column(name = "city")
     private String city;
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getNumberBuilding() {
+        return numberBuilding;
+    }
+
+    public void setNumberBuilding(String numberBuilding) {
+        this.numberBuilding = numberBuilding;
+    }
 
     @Column(name = "street")
     private String street;
